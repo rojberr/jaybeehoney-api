b316f944f5d231a742bbc68fa45976b5208f53bb
rojberr <rojberr@outlook.com>
2022-07-17 18:43:47 +0200

    :rocket: feat(general): Init Spring Boot
    * Add dependencies (see gradle.build)
    * Use Gradle
    * JAva 17 LTS
    
    Relates: #NO-TICKET

---
+++ src/main/java/dev/drzymala/jaybeehoneyapi/JaybeehoneyapiApplication.java
@@ -0,0 +1,13 @@
+package dev.drzymala.jaybeehoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaybeehoneyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaybeehoneyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drzymala/jaybeehoneyapi/JaybeehoneyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drzymala.jaybeehoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaybeehoneyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
