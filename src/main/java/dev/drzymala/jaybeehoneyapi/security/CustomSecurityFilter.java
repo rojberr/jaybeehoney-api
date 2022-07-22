1867e3d2f31f6364493bc5cbdf86d8c0cdcc912f
rojberr <rojberr@outlook.com>
2022-07-22 12:05:10 +0200

    secu(): Allow all GET API calls for now

---
+++ src/main/java/dev/drzymala/jaybeehoneyapi/security/CustomSecurityFilter.java
@@ -0,0 +1,21 @@
+package dev.drzymala.jaybeehoneyapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class CustomSecurityFilter {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .mvcMatchers(HttpMethod.GET, "/**").permitAll()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
