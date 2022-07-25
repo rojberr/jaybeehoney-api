c36ee90cee1a14f2c1a16415e2f468e1b84e1f52
Rojberr <rojberr@outlook.com>
2022-07-25 18:36:00 +0200

    feat(product): Add product init controller

---
+++ src/main/java/dev/drzymala/jaybeehoneyapi/products/web/ProductInitController.java
@@ -0,0 +1,12 @@
+package dev.drzymala.jaybeehoneyapi.products.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/init")
+@AllArgsConstructor
+public class ProductInitController {
+
+}
