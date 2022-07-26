596501003a6247026b19d383fdf1de5874d85e76
Rojberr <rojberr@outlook.com>
2022-07-26 21:21:24 +0200

    domain(orders): Expand ORderItem with additional fields

--- src/main/java/dev/drzymala/jaybeehoneyapi/orders/domain/OrderItem.java
+++ src/main/java/dev/drzymala/jaybeehoneyapi/orders/domain/OrderItem.java
@@ -1,12 +1,18 @@
 package dev.drzymala.jaybeehoneyapi.orders.domain;
 
 import dev.drzymala.jaybeehoneyapi.commons.BaseEntity;
+import dev.drzymala.jaybeehoneyapi.products.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,6 +21,17 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 public class OrderItem extends BaseEntity {
 
-    private String itemName;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    int quantity;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
 }
