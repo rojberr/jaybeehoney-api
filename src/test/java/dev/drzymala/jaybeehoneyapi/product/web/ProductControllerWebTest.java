c8348317e8e7797fa95436f097af0dea6d053f6e
rojberr <rojberr@outlook.com>
2022-07-27 18:39:35 +0200

    test(product): Add Mock cProductController test

---
+++ src/test/java/dev/drzymala/jaybeehoneyapi/product/web/ProductControllerWebTest.java
@@ -0,0 +1,46 @@
+package dev.drzymala.jaybeehoneyapi.product.web;
+
+import dev.drzymala.jaybeehoneyapi.product.application.port.ProductUseCase;
+import dev.drzymala.jaybeehoneyapi.product.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({ProductController.class})
+@ActiveProfiles("test")
+@WithMockUser
+class ProductControllerWebTest {
+
+    @MockBean
+    ProductUseCase products;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldGetAllProducts() throws Exception {
+        // Given
+        Product honey1 = new Product("Name 1");
+        Product honey2 = new Product("Name 2");
+        Mockito.when(products.findAll()).thenReturn(List.of(honey1, honey2));
+
+        // Expect
+        mockMvc.perform(get("/product"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
