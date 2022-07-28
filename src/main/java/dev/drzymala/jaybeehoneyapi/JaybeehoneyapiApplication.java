58b1afc1802ade35ce7b1604bf6a7dadbb660f17
rojberr <rojberr@outlook.com>
2022-07-28 18:37:27 +0200

    conf(order): Add order configurationProp

--- src/main/java/dev/drzymala/jaybeehoneyapi/JaybeehoneyapiApplication.java
+++ src/main/java/dev/drzymala/jaybeehoneyapi/JaybeehoneyapiApplication.java
@@ -1,13 +1,16 @@
 package dev.drzymala.jaybeehoneyapi;
 
+import dev.drzymala.jaybeehoneyapi.order.application.OrderProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OrderProperties.class)
 public class JaybeehoneyapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JaybeehoneyapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JaybeehoneyapiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/dev/drzymala/jaybeehoneyapi/order/application/OrderProperties.java
@@ -0,0 +1,15 @@
+package dev.drzymala.jaybeehoneyapi.order.application;
+
+import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.time.Duration;
+
+@Value
+@ConstructorBinding
+@ConfigurationProperties("app.order")
+public class OrderProperties {
+    Duration paymentPeriod;
+    String abandonCron;
+}
