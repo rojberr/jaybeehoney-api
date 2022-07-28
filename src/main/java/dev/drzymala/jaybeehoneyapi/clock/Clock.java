9e0c71b77b78dbe58f832039ced5d480e8f96250
rojberr <rojberr@outlook.com>
2022-07-28 19:03:53 +0200

    feat(): Add Clock

---
+++ src/main/java/dev/drzymala/jaybeehoneyapi/clock/Clock.java
@@ -0,0 +1,31 @@
+package dev.drzymala.jaybeehoneyapi.clock;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public interface Clock {
+
+    LocalDateTime now();
+
+    class Fake implements Clock {
+
+        private LocalDateTime time;
+
+        public Fake(LocalDateTime time) {
+            this.time = time;
+        }
+
+        public Fake() {
+            this(LocalDateTime.now());
+        }
+
+        @Override
+        public LocalDateTime now() {
+            return time;
+        }
+
+        public void tick(Duration duration) {
+            time = time.plus(duration);
+        }
+    }
+}
